07a25cfc8892a4ae7613e6bc7ff88b6a62165592
Alan Scarpa <alan.scarpa@gmail.com>
2016-03-20 08:55:53 -0400

    added easy animation pod

---
+++ Podclass/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Podclass
+//
+//  Created by Alan Scarpa on 3/20/16.
+//  Copyright © 2016 Podclass. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.setNavigationBarHidden(true, animated: false)
+    }
+
+}
+
